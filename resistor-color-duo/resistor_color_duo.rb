a715a3beb7fd13e9b8ea7eaa71b1128617dadae6
Alex Smirnov <sas2job@gmail.com>
2020-05-17 12:25:29 +0300

    Ref task 3 - 'Resistor Color Duo'

--- resistor-color-duo/resistor_color_duo.rb
+++ resistor-color-duo/resistor_color_duo.rb
@@ -1,8 +1,13 @@
 module ResistorColorDuo
+  # def self.value(input)
+  #   result = []
+  #   colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  #   input.each { |i| result << colors.index(i) }
+  #   result.join("").to_s[0..1].to_i 
+  # end
+
+  COLORS = %w[black brown red orange yellow green blue violet grey white]
   def self.value(input)
-    result = []
-    colors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-    input.each { |i| result << colors.index(i) }
-    result.join("").to_s[0..1].to_i 
+    input.map{ |i| COLORS.index(i) }.join("").to_s[0..1].to_i
   end
 end
