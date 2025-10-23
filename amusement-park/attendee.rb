3ab86c3fe6af415e94b66dfe6f2c230c8f3b3028
Alex Smirnov <sas2job@gmail.com>
2025-10-23 21:05:19 +0300

    Add task 'Amusement Park'

---
+++ amusement-park/attendee.rb
@@ -0,0 +1,22 @@
+class Attendee
+  def initialize(height, pass_id = nil)
+    @height = height
+    @pass_id = pass_id
+  end
+
+  def height
+    @height
+  end
+
+  def pass_id
+    @pass_id
+  end
+
+  def issue_pass!(pass_id)
+    @pass_id = 1
+  end
+
+  def revoke_pass!
+    @pass_id = nil
+  end
+end
